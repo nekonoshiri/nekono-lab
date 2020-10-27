f0d3747d36f4bd39df717f46c642ad35592b6804
nekonoshiri <gexira.halen.toms@gmail.com>
2020-10-27 21:07:03 +0900

    normalize.css をやめて ress.css に変更

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,21 @@
 import React from "react"
 import { Global, css } from "@emotion/core"
-import emotionNormalize from "emotion-normalize"
 import Header from "./header"
 import Footer from "./footer"
+
+import ress from "ress"
 
 export default ({ children }) => (
   <>
     <Global
       styles={css`
-        ${emotionNormalize}
+        ${ress}
+        body {
+          background-color: ivory;
+          font-family: "Helvetica Neue", "Helvetica", "Hiragino Sans",
+            "Hiragino Kaku Gothic ProN", "Arial", "Yu Gothic", "Meiryo",
+            sans-serif;
+        }
       `}
     />
     <Header />
