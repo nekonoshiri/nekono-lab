fc05d0713fa029feaf4348dc7eae4a0871797bb8
nekonoshiri <gexira.halen.toms@gmail.com>
2020-10-27 21:34:44 +0900

    ヘッダーをそれっぽくした

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,40 @@ import React from "react"
 import { Link } from "gatsby"
 import styled from "@emotion/styled"
 
-const Header = styled.header``
+const Header = styled.header`
+  margin: 10px;
+  padding: 10px;
+  background-color: lavender;
+
+  h1 {
+    text-align: center;
+  }
+
+  ul {
+    list-style: none;
+
+    display: flex;
+    justify-content: space-around;
+
+    li {
+      width: 5rem;
+      height: 2rem;
+      &:hover {
+        background-color: orange;
+      }
+    }
+
+    a {
+      color: black;
+      text-decoration: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      width: 100%;
+    }
+  }
+`
 
 export default () => (
   <Header>
