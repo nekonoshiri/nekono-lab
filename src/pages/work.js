d30c36eb712b9d3a2fda055bd8acaa46ca7023c2
nekonoshiri <gexira.halen.toms@gmail.com>
2020-10-28 09:58:29 +0900

    いもむしくんの説明だけ追加

--- src/pages/work.js
+++ src/pages/work.js
@@ -4,6 +4,18 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h2>Work</h2>
-    <p>TODO</p>
+
+    <section>
+      <h3>いもむしくん</h3>
+
+      <figure>
+        <p>TODO: ここに画像</p>
+        <figcaption>いもむしくん</figcaption>
+      </figure>
+
+      <p>
+        いもむしくんは猫野研究所のシンボルです。緑色は世界平和を、黒い点は目を表しています。
+      </p>
+    </section>
   </Layout>
 )
