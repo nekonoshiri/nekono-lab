a3d597957b5b6923acc62a0707f483c5555ab7f0
nekonoshiri <gexira.halen.toms@gmail.com>
2020-10-28 13:41:09 +0900

    フッターに Twitter と GitHub のアイコンを追加

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,31 @@
 import React from "react"
 import styled from "@emotion/styled"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = styled.footer`
   text-align: center;
+  background-color: lightsteelblue;
+
+  p {
+    margin: 10px 0;
+  }
+
+  ul {
+    list-style: none;
+    font-size: 2rem;
+
+    display: flex;
+    justify-content: center;
+
+    li {
+      margin: 10px;
+    }
+
+    a {
+      color: black;
+    }
+  }
 `
 
 export default () => (
@@ -10,5 +33,18 @@ export default () => (
     <p>
       <small>&copy; 2020 Shiri Nekono</small>
     </p>
+
+    <ul>
+      <li>
+        <a href="https://twitter.com/CyLomw">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/nekonoshiri">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </li>
+    </ul>
   </Footer>
 )
