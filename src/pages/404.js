ae73c11df3fea0b61c4d893f92cff9efe1225379
nekonoshiri <gexira.halen.toms@gmail.com>
2020-10-28 13:16:52 +0900

    404ページ追加

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h2>404 Not Found</h2>
+    <p>
+      あなたは真に驚くべきページを見つけましたが、ここに書くには余白が狭すぎます——
+    </p>
+  </Layout>
+)
